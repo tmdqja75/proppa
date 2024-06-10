34b61d015ee056e5e0648590983adaa2e6f38b80
tmdqja75 <tmdqja75@gmail.com>
2024-06-10 22:53:22 +0900

    scrapegraphai-test

--- wanted-webcrawl.py
+++ wanted-webcrawl.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,14 +9,18 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
+from scrapegraphai.graphs import XMLScraperGraph
+
 # Set up the Selenium WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Go to the webpage
-driver.get('https://www.jumpit.co.kr/search?sort=relation&keyword=인공지능')
+# driver.get('https://www.jumpit.co.kr/search?sort=relation&keyword=인공지능')
+driver.get('https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=job.recommend_order&years=-1&selected=1024&selected=1634&locations=all')
+
 
 # Scroll and load more job postings
-SCROLL_PAUSE_TIME = 1.5
+SCROLL_PAUSE_TIME = 0.5
 
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
@@ -32,11 +38,64 @@ while True:
         break
     last_height = new_height
 
-# # Parse the page source with BeautifulSoup
-# soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# # Close the Selenium WebDriver
-# driver.quit()
+
+# Parse the page source with BeautifulSoup
+page_source = driver.page_source
+# Convert to XML
+# page_source = page_source.prettify(formatter="xml")
+
+# Close the Selenium WebDriver
+driver.quit()
+
+# print(page_source)
+
+with open('page_source.xml', 'w', encoding='utf-8') as file:
+    file.write(page_source)
+
+# Define the graph configuration
+
+# ollama configuration
+# # from https://www.scrapingbee.com/blog/scrapegraph-ai-tutorial-scrape-websites-easily-with-llama-ai/
+# graph_config = {
+#     "llm": {
+#         "model": "ollama/llama3",  # Specifies the large language model to use
+#         "temperature": 0,          # Temperature controls randomness; 0 makes it deterministic
+#         "format": "json",          # Output format is set to JSON
+#         "base_url": "http://localhost:11434",  # Base URL where the Ollama server is running
+#     },
+#     "embeddings": {
+#         "model": "ollama/nomic-embed-text",  # Specifies the embedding model to use
+#         "temperature": 0,                    # Keeps the generation deterministic
+#         "base_url": "http://localhost:11434",  # Base URL for the embeddings model server
+#     },
+#     "verbose": True
+# }
+
+# OpenAI configuration
+graph_config = {
+    "llm": {
+        "api_key": os.environ.get('OPENAI_API_KEY'),
+        "temperature": 0,
+        "format": "json",
+        "model": "gpt-4-turbo", 
+    },
+    # "embeddings": {
+    #     "model": "ollama/nomic-embed-text",  
+    #     "temperature": 0,                    
+    #     "base_url": "http://localhost:11434",
+    # },
+    "verbose": True
+}
+
+graph = XMLScraperGraph(
+    prompt='scrape all job posting uri from the given xml file.',
+    source=page_source,
+    config=graph_config,
+)
+
+result = graph.run()
+print(result)
 
 # # Extract job links
 # job_list = soup.find('ul', {'data-cy': 'job-list'})
